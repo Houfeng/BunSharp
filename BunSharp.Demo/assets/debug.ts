b59863b4366a0577f57397f41f95bd07a41d63ab
Houfeng <admin@xhou.net>
2026-04-03 15:05:23 +0800

    chore: 增加 JS 调试配置

--- BunSharp.Demo/assets/debug.ts
+++ BunSharp.Demo/assets/debug.ts
@@ -1,5 +1,3 @@
-declare function helloFromDotNet(name: string): string;
-
 declare class DemoGreeter {
   constructor(name: string, payload: Uint8Array);
   static version: string;
@@ -7,38 +5,7 @@ declare class DemoGreeter {
   describe(): string;
 }
 
-console.log("debug.ts loaded");
+debugger;
 
-const debugState = globalThis as typeof globalThis & {
-  __debugCounter?: number;
-  __debugProbeTick?: number;
-};
-
-let didRunDemo = false;
-let remainingProbes = 20;
-
-const probe = setInterval(() => {
-  debugState.__debugProbeTick = (debugState.__debugProbeTick ?? 0) + 1;
-  console.log("debug probe:", debugState.__debugProbeTick);
-
-  debugger;
-
-  if (!didRunDemo) {
-    didRunDemo = true;
-
-    const greeting = helloFromDotNet("debug.ts");
-    console.log("greeting:", greeting);
-
-    const greeter = new DemoGreeter("Debugger", new Uint8Array([1, 2, 3, 4]));
-    console.log("greeter:", greeter.describe(), greeter.name, DemoGreeter.version);
-
-    debugState.__debugCounter = (debugState.__debugCounter ?? 0) + 1;
-    console.log("counter:", debugState.__debugCounter);
-  }
-
-  remainingProbes -= 1;
-  if (remainingProbes <= 0) {
-    clearInterval(probe);
-    console.log("debug probes finished");
-  }
-}, 1000);
+const greeter = new DemoGreeter("Debugger", new Uint8Array([1, 2, 3, 4]));
+console.log("greeter:", greeter.describe(), greeter.name, DemoGreeter.version);
